905358960db538aa43e649da3908674bf7de6c9c
moritzd <moritz.daniel@gmail.com>
2014-03-23 15:17:24 +0100

    tried to use routes

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,13 +2,25 @@
 
 var tacApp = angular.module('tacApp', []);
 
+tacApp.config(function($routeProvider){
+	$routeProvider.
+		when('list', {
+			controller: SlidesCtrl,
+			templateUrl: 'app/partials/list.html'
+		}).
+		otherwise({
+			controller: SlidesCtrl,
+			templateUrl: 'partials/list.html'
+	});
+});
+
 tacApp.controller('SlidesCtrl', function($scope){
 
 	$scope.slides = [
 		{"number": 1,
 		 "title": "Introduction",
 		 "content": "History of AngularJS",
-		 "isEdited": false
+		 "isEdited": true
 		},
 		{"number": 2,
 		 "title": "Template",
@@ -17,16 +29,29 @@ tacApp.controller('SlidesCtrl', function($scope){
 		}
 	];
 
-	var resetOtherEditFlags = function(){
-		for (slide in slides){
-			if 	(slide == currentSlide)
-				slide.isEdited = true;
-			else {
-				slide.isEdited = false,
+	$scope.currentSlide = $scope.slides[0];
+
+	var setCurrentSlide = function(){
+		console.log("I am here");
+		foundit = false;
+		for (var i = $scope.slides.length - 1; i >= 0; i--) {
+			if ($scope.slides[i].isEdited == true){
+				if (foundit) {
+				$scope.slides[i].isEdited = false;
+			} else {
+				foundit=true;
+				$scope.currentSlide = $scope.slides[i];
 			}
-		}
+			}
+		};
 		
 	};
 
-	$scope.currentSlide=slides[0];
+	$scope.$watch('slides', setCurrentSlide, true);
+
 });
+
+
+
+	//$scope.currentSlide=$scope.slides[0];
+
